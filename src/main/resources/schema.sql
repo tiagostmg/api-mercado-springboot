62a3f2442407b471c4231fc44796f4d291ef322f
tiagostmg <tiago.taumaturgo@gmail.com>
2025-05-06 14:29:38 -0300

    fixed h2

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,22 +1,26 @@
+DROP TABLE IF EXISTS purchase;
+DROP TABLE IF EXISTS client;
+DROP TABLE IF EXISTS product;
+
 CREATE TABLE client (
-                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
                         name VARCHAR(255) NOT NULL,
                         balance DECIMAL(10, 2)
 );
 
 CREATE TABLE product (
-                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
                          name VARCHAR(255) NOT NULL,
                          price DECIMAL(10, 2) NOT NULL,
                          quantity INT NOT NULL
 );
 
 CREATE TABLE purchase (
-                          id INT AUTO_INCREMENT PRIMARY KEY,
-                          client_id INT NOT NULL,
-                          product_id INT NOT NULL,
+                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          client_id BIGINT NOT NULL,
+                          product_id BIGINT NOT NULL,
                           quantity INT NOT NULL,
-                          purchase_date TIMESTAMP NOT NULL,
+                          date TIMESTAMP NOT NULL,
                           FOREIGN KEY (client_id) REFERENCES client(id),
                           FOREIGN KEY (product_id) REFERENCES product(id)
 );
